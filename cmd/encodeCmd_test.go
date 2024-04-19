24435d72bf7a5e10855524bfd7c93bf789273467
David Ulicny <ulicny.david@gmail.com>
2024-04-19 16:43:38 +0200

    fix test

--- cmd/encodeCmd_test.go
+++ cmd/encodeCmd_test.go
@@ -19,5 +19,5 @@ func TestEncode(t *testing.T) {
 	encodeCmd.SetOut(b)
 	encodeCmd.SetErr(b)
 	encodeCmd.Run(encodeCmd, []string{"{\"test\":\"test\"}"})
-	assert.Equal(t, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoidGVzdCJ9.b5o0a23g5hGLIIcaqpfX3-oOuJVVRalv7xwkXRuHPG8\n", b.String())
+	assert.Equal(t, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoidGVzdCJ9.rcu2besU6jKCzZk6sVEpfkDFWftv7zYjFT6jAxPYGFk\n", b.String())
 }
